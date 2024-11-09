6c8d17a6cadcd1f18a30b6727f4f2d80e86c7393
Rafael Franca <rafa-cross@hotmail.com>
2024-11-08 23:56:53 -0300

    feat: add card-list to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,26 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
+import { LabelComponent } from './components/label/label.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FormsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormsComponent,
+    CardsListComponent,
+    CardComponent,
+    LabelComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
